05a0603748e9882aea60314eee9365f3267051d4
Nakshatra Sachan <nsachan2002@gmail.com>
2025-07-30 23:54:08 +0530

    Added cors package

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express(); 
+app.use(cors());
 app.use(express.json());
 
 const { Pool } = require('pg');
